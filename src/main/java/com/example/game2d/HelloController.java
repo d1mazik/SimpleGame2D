642ad023b02ef79207d2e3532a1c754e6a49f30b
Dmitri Gustafsson <dima.gustafsson@hotmail.com>
2025-01-30 10:38:22 +0100

    Fixed background scrolling and player visibility issue

--- src/main/java/com/example/game2d/HelloController.java
+++ src/main/java/com/example/game2d/HelloController.java
@@ -1,14 +1,49 @@
 package com.example.game2d;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView bg1, bg2, player;
+
+    private final int BG_WIDTH = 711;
+
+    private ParallelTransition parallelTransition;
+
+    @FXML
+    void initialize() {
+
+        TranslateTransition bgOneTransition = new TranslateTransition(Duration.millis(5000), bg1);
+        bgOneTransition.setFromX(0);
+        bgOneTransition.setToX(BG_WIDTH * -1);
+        bgOneTransition.setInterpolator(Interpolator.LINEAR);
+
+
+        TranslateTransition bgTwoTransition = new TranslateTransition(Duration.millis(5000), bg2);
+        bgTwoTransition.setFromX(0);
+        bgTwoTransition.setToX(BG_WIDTH * -1);
+        bgTwoTransition.setInterpolator(Interpolator.LINEAR);
+
+        parallelTransition = new ParallelTransition(bgOneTransition, bgTwoTransition);
+        parallelTransition.setCycleCount(Animation.INDEFINITE);
+        parallelTransition.play();
+        player.toFront();
     }
+
 }
